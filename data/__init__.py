0033277f5ca41c5e823dc1751e13f3b2c1d46446
Huicong(Hugo) Ding <dinghc@vip.qq.com>
2014-11-23 16:00:41 +0800

    Add data template for testcase

---
+++ data/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding=utf-8 -*-
+
+"""
+About this module
+
+Description of classes
+
+Description of methods
+
+"""
+
+__authors__ = [
+    '"Hugo Ding" <huicong.ding@spirent.com>',
+]
+
+__version__ = "V0.1"
+
+__all__ = []
+
+# ChangeLog:
+# Version   Date         Description                                   Author
+# ------------------------------------------------------------------------------
+# V0.1      xxxx-xx-xx   First version                                 Hugo
+# ------------------------------------------------------------------------------
